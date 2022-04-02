2268c1c8564d491ef7548b59989031b2469a4f9a
findoc2021 <findoc2021@gmail.com>
2022-04-02 17:01:02 +0300

    1 file modified

--- shopify.config.js
+++ shopify.config.js
@@ -3,16 +3,3 @@ export default {
   storefrontToken: '88dc4e4d82855b35d29d4e17d8051da9',
   storefrontApiVersion: '2022-04',
 };
-
-import {useNavigate} from '@shopify/hydrogen/client';
-
-function addToCart() {}
-
-export default function ClientComponent() {
-  const navigate = useNavigate();
-  async function clickAddToCart() {
-    await addToCart();
-    navigate('/success', {replace: true});
-  }
-  return <Button onClick={clickAddToCart}>Add to Cart</Button>;
-}

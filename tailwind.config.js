882489f0255f6708f813e00b6a7d0bae50b04fdd
findoc2021 <findoc2021@gmail.com>
2022-04-02 17:02:09 +0300

    1 file modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,24 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            hr: {
-              borderColor: theme('colors.gray.200'),
-              borderTopWidth: '1px',
-              marginTop: '2rem',
-              marginBottom: '2rem',
-            },
-            'ol > li::before': {
-              color: theme('colors.gray.900'),
-            },
-            'ul > li::before': {
-              backgroundColor: theme('colors.gray.900'),
-            },
-          },
-        },
-      }),
-    },
-  },
-  plugins: [require('@tailwindcss/typography')],
+export const content=['./index.html','./src/**/*.{js,jsx,ts,tsx}'];
+export const theme={
+extend: {
+typography: (theme) => ({
+DEFAULT: {
+css: {
+hr: {
+borderColor: theme('colors.gray.200'),
+borderTopWidth: '1px',
+marginTop: '2rem',
+marginBottom: '2rem',
+},
+'ol > li::before': {
+color: theme('colors.gray.900'),
+},
+'ul > li::before': {
+backgroundColor: theme('colors.gray.900'),
+},
+},
+},
+}),
+},
 };
+export const plugins=[require('@tailwindcss/typography')];
